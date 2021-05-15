00db73c589ed5ba05773bbf44ffadb940036e4cd
Lars Ivar Hatledal <laht@hials.no>
2021-05-15 09:44:22 +0200

    update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ## OpenCV C++ example
+
+Invoke `run_conan_install.sh` prior to configuring and building the project.
--- run_conan_install.sh
+++ run_conan_install.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-conan remote add osp https://osp.jfrog.io/artifactory/api/conan/conan-local -f
-
 unameOut="$(uname -s)"
 case "${unameOut}" in
     MINGW*)     conan install . -s build_type=Release --install-folder=cmake-build-release --build=missing;;
