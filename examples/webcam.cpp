65cd3b98620ca75f498685c39a68bf3040c78608
Lars Ivar Hatledal <laht@ntnu.no>
2021-05-18 09:20:46 +0200

    update

--- examples/webcam.cpp
+++ examples/webcam.cpp
@@ -1,6 +1,9 @@
+
 #include "opencv2/highgui.hpp"
 #include "opencv2/objdetect.hpp"
+
 #include <iostream>
+#include <thread>
 
 #include "detect_face.hpp"
 
@@ -8,17 +11,9 @@ using namespace cv;
 
 int main() {
 
-    Mat frame, image;
+    namedWindow("Display Image", WINDOW_AUTOSIZE);
+
     CascadeClassifier cascade, nestedCascade;
-
-    std::string inputName("../images/Lenna.png");
-
-    image = imread(inputName, IMREAD_COLOR);
-    if (image.empty()) {
-        std::cout << "Could not read " << inputName << std::endl;
-        return 1;
-    }
-
     std::string cascadeName("../cascades/haarcascades/haarcascade_frontalface_alt.xml");
     std::string nestedCascadeName("../cascades/haarcascades/haarcascade_eye_tree_eyeglasses.xml");
 
@@ -29,10 +24,20 @@ int main() {
         return -1;
     }
 
-    std::cout << "Detecting face(s) in " << inputName << std::endl;
-    detect(image, cascade, nestedCascade);
-    imshow("result", image);
-    waitKey(0);
+    bool stop = false;
+    std::thread t([&stop] {
+      waitKey(0);
+      stop = true;
+    });
 
-    return 0;
+    Mat image;
+    VideoCapture capture(0);
+
+    while (!stop && capture.isOpened()) {
+        capture >> image;
+        detect(image, cascade, nestedCascade);
+        imshow("Display Image", image);
+    }
+
+    t.join();
 }
