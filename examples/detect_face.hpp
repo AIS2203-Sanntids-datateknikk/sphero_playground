9cb1395d2dcf2e5fd9cf994d3bd6961c4f866a72
Lars Ivar Hatledal <laht@ntnu.no>
2021-05-19 09:16:26 +0200

    refactor

--- examples/detect_face.hpp
+++ examples/detect_face.hpp
@@ -9,42 +9,39 @@
 #include <iostream>
 #include <optional>
 
-using namespace cv;
-
 namespace
 {
 
-std::optional<Point> detect_face(
-    Mat& img,
-    CascadeClassifier& cascade,
-    CascadeClassifier& nestedCascade)
+std::optional<cv::Point> detect_face(
+    cv::Mat& img,
+    cv::CascadeClassifier& cascade,
+    cv::CascadeClassifier& nestedCascade)
 {
     double t;
-    std::vector<Rect> faces;
-    Mat gray, smallImg;
-    cvtColor(img, gray, COLOR_BGR2GRAY);
-    resize(gray, smallImg, Size(), 1, 1, INTER_LINEAR_EXACT);
+    std::vector<cv::Rect> faces;
+    cv::Mat gray, smallImg;
+    cvtColor(img, gray, cv::COLOR_BGR2GRAY);
+    resize(gray, smallImg, cv::Size(), 1, 1, cv::INTER_LINEAR_EXACT);
     equalizeHist(smallImg, smallImg);
-    t = (double)getTickCount();
-    cascade.detectMultiScale(smallImg, faces,
+    t = (double)cv::getTickCount();
+    cascade.detectMultiScale(gray, faces,
         1.1, 2, 0
             //|CASCADE_FIND_BIGGEST_OBJECT
             //|CASCADE_DO_ROUGH_SEARCH
-            | CASCADE_SCALE_IMAGE,
-        Size(30, 30));
-    t = (double)getTickCount() - t;
-    printf("detection time = %g ms\n", t * 1000 / getTickFrequency());
-
+            | cv::CASCADE_SCALE_IMAGE,
+        cv::Size(30, 30));
+    t = (double)cv::getTickCount() - t;
+    printf("detection time = %g ms\n", t * 1000 / cv::getTickFrequency());
 
     if (faces.empty()) {
         return std::nullopt;
     }
 
-    Point center;
-    Rect r = faces[0];
-    Mat smallImgROI;
-    std::vector<Rect> nestedObjects;
-    Scalar color = Scalar(255, 0, 0);
+    cv::Point center;
+    cv::Rect r = faces[0];
+    cv::Mat smallImgROI;
+    std::vector<cv::Rect> nestedObjects;
+    cv::Scalar color = cv::Scalar(255, 0, 0);
     int radius;
     double aspect_ratio = (double)r.width / r.height;
     if (0.75 < aspect_ratio && aspect_ratio < 1.3) {
@@ -53,8 +50,8 @@ std::optional<Point> detect_face(
         radius = cvRound((r.width + r.height) * 0.25);
         circle(img, center, radius, color, 3, 8, 0);
     } else
-        rectangle(img, Point(cvRound(r.x), cvRound(r.y)),
-            Point(cvRound((r.x + r.width - 1)), cvRound((r.y + r.height - 1))),
+        rectangle(img, cv::Point(cvRound(r.x), cvRound(r.y)),
+            cv::Point(cvRound((r.x + r.width - 1)), cvRound((r.y + r.height - 1))),
             color, 3, 8, 0);
     if (!nestedCascade.empty()) {
         smallImgROI = smallImg(r);
@@ -63,8 +60,8 @@ std::optional<Point> detect_face(
                 //|CASCADE_FIND_BIGGEST_OBJECT
                 //|CASCADE_DO_ROUGH_SEARCH
                 //|CASCADE_DO_CANNY_PRUNING
-                | CASCADE_SCALE_IMAGE,
-            Size(30, 30));
+                | cv::CASCADE_SCALE_IMAGE,
+            cv::Size(30, 30));
 
         for (const auto& nr : nestedObjects) {
             center.x = cvRound((r.x + nr.x + nr.width * 0.5));
