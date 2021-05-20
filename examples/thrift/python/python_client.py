3a9b79ae3aacce4c80ce03c31ab46aa08037532c
Lars Ivar Hatledal <laht@ntnu.no>
2021-05-20 10:56:08 +0200

    update

--- examples/thrift/python/python_client.py
+++ examples/thrift/python/python_client.py
@@ -1,3 +1,4 @@
+import time
 
 from service import ImageService
 
@@ -22,16 +23,24 @@ def main():
         img = cv.imread("../../../images/Lenna.png")
         encode = cv.imencode(".jpg", img)[1].tostring()
 
-        detect = client.detect_face(encode)
-        if detect.detected:
+        while True:
 
-            rect = detect.rect
-            cv.rectangle(img, (rect.x, rect.y), (rect.x+rect.width, rect.y + rect.height), (255, 0, 0), 3, 8)
-            cv.imshow('image', img)
+            start = time.time()
+            detect = client.detect_face(encode)
+            end = time.time()
+            print(f"Image detection took {end - start}s")
 
-            cv.waitKey(0)
-        else:
-            print("No image detected")
+            if detect.detected:
+
+                rect = detect.rect
+                cv.rectangle(img, (rect.x, rect.y), (rect.x+rect.width, rect.y + rect.height), (255, 0, 0), 3, 8)
+                cv.imshow('image', img)
+
+                if cv.waitKey(1) == 27:  # press esc to quit
+                    break
+            else:
+                print("No image detected")
+
         # Close!
         transport.close()
     except Exception as e:
